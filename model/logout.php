959bc72ae78bad810cceacb85d1a27d153cb0516
root <root@LAPTOP-PI4MQAGH>
2024-02-09 13:13:39 +0100

    logout_sans_probleme

--- model/logout.php
+++ model/logout.php
@@ -3,15 +3,10 @@
     $MyDatabase = require_once("class.php");
     $sessionId = $_COOKIE['session'];
     if($sessionId){
-        // $statement_logout = $pdo->prepare("SELECT * FROM session WHERE id_session = '$sessionId';");
-        // $statement_logout->execute();
-        // $session = $statement->fetchAll(PDO::FETCH_ASSOC);
         $session = $MyDatabase->logout($sessionId);
         if($session){
             setcookie('session');
             unset($_COOKIE['session']);
-            // unset($_COOKIE['signature']);
-            // setcookie('signature', time() - 1);
             header("Location: ../index.php");
             exit;
         }
